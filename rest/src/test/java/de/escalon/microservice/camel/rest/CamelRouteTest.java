b3312a5729667fe3836629c0a9099ebf860ca7d2
Dietrich Schulten <ds@escalon.de>
2020-04-06 06:57:36 +0200

    Added RestAssured test with spring-restdocs

--- rest/src/test/java/de/escalon/microservice/camel/rest/CamelRouteTest.java
+++ rest/src/test/java/de/escalon/microservice/camel/rest/CamelRouteTest.java
@@ -1,5 +1,7 @@
 package de.escalon.microservice.camel.rest;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ServiceStatus;
@@ -9,16 +11,25 @@ import org.apache.camel.impl.engine.DefaultFluentProducerTemplate;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+import static io.restassured.RestAssured.given;
 import static org.apache.camel.builder.Builder.constant;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,6 +37,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @CamelSpringBootTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = "spring.cloud.consul.enabled=false")
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public class CamelRouteTest {
 
   @Autowired
@@ -39,8 +51,13 @@ public class CamelRouteTest {
 
   private User user;
 
+  private RequestSpecification documentationSpec;
+
+  @LocalServerPort
+  private int port;
+
   @BeforeEach
-  public void setUp() throws Exception {
+  public void setUp(RestDocumentationContextProvider restDocumentation) throws Exception {
     AdviceWithRouteBuilder.adviceWith(camelContext, "getUsersRoute", a -> {
       a.mockEndpointsAndSkip("bean:userService*");
     });
@@ -50,6 +67,9 @@ public class CamelRouteTest {
     user.setName("Jane");
 
     mockUserService.returnReplyBody(constant(new User[] {user}));
+
+    this.documentationSpec = new RequestSpecBuilder()
+        .addFilter(documentationConfiguration(restDocumentation)).build();
   }
 
   @Test
@@ -66,6 +86,22 @@ public class CamelRouteTest {
         .to("direct:getusers")
         .request(User[].class);
     assertThat(users).contains(user);
+  }
+
+  @Test
+  public void restAssuredGeneratesRestdocs() throws Exception {
+    given(this.documentationSpec)
+        .accept("text/plain")
+        .filter(document("sample",
+            preprocessRequest(modifyUris()
+                .scheme("https")
+                .host("api.example.com")
+                .removePort())))
+        .when()
+        .port(this.port)
+        .get("/rest/users")
+        .then()
+        .assertThat().statusCode(is(200));
   }
 
   @Test
